c62c686359e134e07624cb8e7af7ba53839f820a
elihinton <a1923292@adelaide.edu.au>
2025-09-17 23:52:09 +0930

    edit sort

--- ArrSort/ArrSort.asm
+++ ArrSort/ArrSort.asm
@@ -3,95 +3,106 @@
 
 // Put your code here.
 
+@R2
+D=M
+@1
+D=D-A
+@DONE
+D;JLE
+
 @R1
 D=M
+@R6
+M=D
+
+@R2
+D=M
+@R5
+M=D
+
+@0
+D=A
 @R3
 M=D
 
-(OUTER)
-    @R3
-    D=M
-    @R1
-    D=D-M
-    @R2
-    D=M-D
-    @DONE
-    D;JLE
-
-    // min = i
+(OUTER_LOOP)
     @R3
     D=M
     @R5
-    M=D
-
-    @R3
-    D=M
+    D=D-M
     @1
     D=D+A
-    @R4
-    M=D
-
-(INNER)
-    @R4
-    D=M
-    @R1
-    D=D-M
-    @R2
-    D=M-D
-    @END_INNER
+    @DONE
     D;JGE
 
+    @0
+    D=A
     @R4
-    A=M
-    D=M
-    @R6
     M=D
-    @R5
-    A=M
-    D=M
-    @SKIP
-    D=D-M
-    @SKIP
-    D;JLE
-    @R4
-    D=M
-    @R5
-    M=D
-(SKIP)
+
+    (INNER_LOOP)
+        @R4
+        D=M
+        @R5
+        D=M-D
+        @R3
+        D=D-M
+        @1
+        D=D-A
+        @END_INNER
+        D;JLE
+
+        @R6
+        D=M
+        @R4
+        A=D+M
+        D=M
+        @R7
+        M=D
+
+        @R6
+        D=M
+        @R4
+        A=D+M
+        A=A+1
+        D=M
+        @R8
+        M=D
+
+        @R7
+        D=M
+        @R8
+        D=D-M
+        @NOSWAP
+        D;JLE
+
+        @R8
+        D=M
+        @R6
+        A=M
+        @R4
+        A=A+M
+        M=D
+
+        @R7
+        D=M
+        @R6
+        A=M
+        @R4
+        A=A+M
+        A=A+1
+        M=D
+
+(NOSWAP)
     @R4
     M=M+1
-    @INNER
+    @INNER_LOOP
     0;JMP
 
 (END_INNER)
-    @R5
-    D=M
-    @R3
-    D=D-M
-    @NOSWAP
-    D;JEQ
-
-    @R3
-    A=M
-    D=M
-    @R6
-    M=D
-    @R5
-    A=M
-    D=M
-    @R3
-    A=M
-    M=D
-    @R6
-    D=M
-    @R5
-    A=M
-    M=D
-(NOSWAP)
-    // i++
     @R3
     M=M+1
-    @OUTER
+    @OUTER_LOOP
     0;JMP
 
 (DONE)
