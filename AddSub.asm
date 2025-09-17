089d59b2a50cf7c7175eb38fd960845dbfc39782
elihinton <a1923292@adelaide.edu.au>
2025-09-17 16:10:52 +0930

    init

---
+++ AddSub.asm
@@ -0,0 +1,15 @@
+// Calculates R1 + R2 - R3 and stores the result in R0.
+// (R0, R1, R2, R3 refer to RAM[0], RAM[1], RAM[2], and RAM[3], respectively.)
+
+// Put your code here.
+@R1
+D=R1
+
+@R2
+D=D+R2
+
+@R3
+D=D-R3
+
+@R0
+R0=D
